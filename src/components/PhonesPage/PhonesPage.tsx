d485fb8ce7758ca10b41f3ba5be2c87d84ee6aa7
mitattt <garkavivii@gmail.com>
2023-01-07 04:29:50 +0200

    card layout

--- src/components/PhonesPage/PhonesPage.tsx
+++ src/components/PhonesPage/PhonesPage.tsx
@@ -1,5 +1,8 @@
 import { ProductCard } from '../ProductCard';
 
 export const PhonesPage: React.FC = () => (
-  <ProductCard />
+  <>
+    <ProductCard />
+    <h1 className="page__title">Phones page</h1>
+  </>
 );

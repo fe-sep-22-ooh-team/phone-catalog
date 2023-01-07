ef6a6faa3847f9aeeda404fa1bc61e8dd62d5db3
mitattt <garkavivii@gmail.com>
2023-01-07 04:24:53 +0200

    card layout

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/button-has-type */
+import React, { useState } from 'react';
+
+import classnames from 'classnames';
+import styles from './Button.module.scss';
+import { Phone } from '../../types/Phone';
+
+type Props = {
+  phone?: Phone,
+};
+
+export const Button: React.FC<Props> = () => {
+  const [select, setSelect] = useState(false);
+
+  const handleCartButtonClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    setSelect(true);
+  };
+
+  return (
+    <button
+      onClick={handleCartButtonClick}
+      className={classnames(styles.btn, {
+        [styles.btn_selected]: select,
+      })}
+    >
+      { select ? 'Added' : 'Add to cart' }
+    </button>
+  );
+};

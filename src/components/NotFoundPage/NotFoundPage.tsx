5c1987c58b72725d0b84f5e56bc531400ab72fa1
Anna Shapovalovaemail <3809.kasabian.00@gmail.com>
2023-01-07 02:41:14 +0200

    footer fixed && some pages was added

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 export const NotFoundPage: React.FC = () => (
   <div className="grid__item--tablet--1-9 grid__item--desktop--1-24">
-    <h1 className="page__subtitle">
-      Ooops...
-    </h1>
+    <h1 className="page__subtitle">Ooops...</h1>
   </div>
 );
--- src/components/TabletsPage/TabletsPage.tsx
+++ src/components/TabletsPage/TabletsPage.tsx
@@ -4,9 +4,7 @@ import cartEmpty from '../../assets/img/Cart-empty.svg';
 
 export const TabletsPage: React.FC = () => (
   <div className="grid__item--tablet--1-9 grid__item--desktop--1-24">
-    <h1 className="page__subtitle">
-      Sorry, no products here yet
-    </h1>
+    <h1 className="page__subtitle">Sorry, no products here yet</h1>
     <img src={cartEmpty} alt="CartEmpty" />
   </div>
 );

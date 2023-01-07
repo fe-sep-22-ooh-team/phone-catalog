af74633144b40edf5cbaf1360652a3781c7edfcc
mitattt <garkavivii@gmail.com>
2023-01-07 08:28:03 +0200

    card layout was finished + some fixes in the project

--- src/components/PhonesPage/PhonesPage.tsx
+++ src/components/PhonesPage/PhonesPage.tsx
@@ -3,6 +3,5 @@ import { ProductCard } from '../ProductCard';
 export const PhonesPage: React.FC = () => (
   <>
     <ProductCard />
-    <h1 className="page__title">Phones page</h1>
   </>
 );
--- src/components/TabletsPage/TabletsPage.tsx
+++ src/components/TabletsPage/TabletsPage.tsx
@@ -3,8 +3,12 @@ import React from 'react';
 import cartEmpty from '../../assets/img/Cart-empty.svg';
 
 export const TabletsPage: React.FC = () => (
-  <div className="grid__item--tablet--1-9 grid__item--desktop--1-24">
-    <h1 className="page__subtitle">Sorry, no products here yet</h1>
+  <div className="
+  grid__item--mobile--2-4
+  grid__item--tablet--4-9
+  grid__item--desktop--8-24"
+  >
+    <h1 className="page__subtitle">There are no tablets yet</h1>
     <img src={cartEmpty} alt="CartEmpty" />
   </div>
 );

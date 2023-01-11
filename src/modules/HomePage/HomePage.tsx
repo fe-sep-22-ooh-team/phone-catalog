7bc43738196f617ffeeea6dbbc7517a41858ba6e
mitattt <garkavivii@gmail.com>
2023-01-11 11:53:56 +0200

    About and techSpecs section

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -17,36 +17,46 @@ export const HomePage: React.FC = () => {
     <div className={styles.homePage__container}>
       <div className="grid">
         <div
-          className="grid__item--mobile--1-4
-        grid__item--tablet--1-9 grid__item--desktop--1-24"
+          className="
+          grid__item--mobile--1-4
+          grid__item--tablet--1-9
+          grid__item--desktop--1-24"
         >
           <h1 className="page__title">Welcome to Nice Gadgets store!</h1>
         </div>
 
         <div
-          className="grid__item--mobile--1-4
-        grid__item--tablet--1-12 grid__item--desktop--1-24"
+          className="
+          grid__item--mobile--1-4
+          grid__item--tablet--1-12
+          grid__item--desktop--1-24"
         >
           <Slider />
         </div>
 
         <div
-          className="grid__item--mobile--1-4
-        grid__item--tablet--1-12 grid__item--desktop--1-24"
+          className="
+          grid__item--mobile--1-4
+          grid__item--tablet--1-12
+          grid__item--desktop--1-24"
         >
           <Featured phones={newModels} title="Brand new models" />
         </div>
 
         <div
-          className="grid__item--mobile--1-4
-        grid__item--tablet--1-12 grid__item--desktop--1-24"
+          className="
+          grid__item--mobile--1-4
+          grid__item--tablet--1-12
+          grid__item--desktop--1-24"
         >
           <Categories categories={categoriesFromServer} />
         </div>
 
         <div
-          className="grid__item--mobile--1-4
-        grid__item--tablet--1-12 grid__item--desktop--1-24"
+          className="
+          grid__item--mobile--1-4
+          grid__item--tablet--1-12
+          grid__item--desktop--1-24"
         >
           <Featured phones={hotPrices} title="Hot prices" />
         </div>

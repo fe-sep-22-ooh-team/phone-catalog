e88367b4990fcf7058d30e218d98c1f6f2a61c3f
Roman_Maksymuk <romamaksymuk@gmail.com>
2023-01-13 10:17:24 +0200

    added new loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,77 @@
-import React from 'react';
+import { motion } from 'framer-motion';
 import styles from './Loader.module.scss';
 
-export const Loader: React.FC = () => (
-  <div className={styles.loader}>
-    <div className={styles.loader__content} />
-  </div>
-);
+const loadingContainer = {
+  width: '4rem',
+  height: '4rem',
+  display: 'flex',
+  justifyContent: 'space-around',
+};
+const loadingCircle = {
+  display: 'block',
+  width: '1rem',
+  height: '1rem',
+  backgroundColor: '#89939a',
+  borderRadius: '0.5rem',
+};
+
+const loadingContainerVariants = {
+  start: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const loadingCircleVariants = {
+  start: {
+    y: '0%',
+  },
+  end: {
+    y: '60%',
+  },
+};
+const loadingCircleTransition = {
+  duration: 0.4,
+  yoyo: Infinity,
+  ease: 'easeInOut',
+};
+
+export const Loader = () => {
+  return (
+    <div className={styles.loader}>
+      <div className="fixed  w-full min-h-screen z-50 bg-black opacity-30" />
+      <div className="flex fixed w-full justify-center items-center h-screen">
+        <motion.div
+          style={loadingContainer}
+          variants={loadingContainerVariants}
+          initial="start"
+          animate="end"
+        >
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          />
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          />
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
